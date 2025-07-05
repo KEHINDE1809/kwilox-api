10cab7412ba8c79fadd83f40b71d3cbe0afaa8f2
Adara Kehinde Solomon <askallgist@gmail.com>
2025-07-05 22:09:26 +0100

    Updated getDrinkById endpoint

--- controllers/drinkController.js
+++ controllers/drinkController.js
@@ -4,13 +4,28 @@ const Drink = require('../models/Drink');
 exports.createDrink = async (req, res) => {
   try {
     const { name, manufacturer, expiryDate, quantity } = req.body;
+    
+    const existingDrink = await Drink.findOne({ name, manufacturer });
+    
+    if (existingDrink) {
+      return res.status(400).json({ message: 'Drink already exists' });
+    }
     const drink = await Drink.create({ name, manufacturer, expiryDate, quantity });
-    res.status(201).json(drink);
+    return res.status(201).json(drink); 
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+// get product by ID//
+exports.getDrinkById = async (req, res) => {
+  try {
+    const drink = await Drink.findById(req.params.id);
+    if (!drink) return res.status(404).json({ message: 'Drink not found' });
+    res.json(drink);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
-
 // Get all drinks
 exports.getAllDrinks = async (req, res) => {
   try {
--- routes/drinkRoutes.js
+++ routes/drinkRoutes.js
@@ -6,5 +6,7 @@ router.post('/drinks', drinkController.createDrink);
 router.get('/drinks', drinkController.getAllDrinks);
 router.put('/drinks/:id', drinkController.updateDrink);
 router.delete('/drinks/:id', drinkController.deleteDrink);
+router.get('/drinks/:id', drinkController.getDrinkById);
 
 module.exports = router;
+
